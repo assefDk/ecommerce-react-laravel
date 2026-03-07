0c8092ebc20bf4ccd65af6b32a472c5e2f95d1e3
assefDK <assefdozkanji@gmail.com>
2026-03-07 13:31:48 +0300

    Initial project setup (React frontend + Laravel backend)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home'
+import Shop from './components/Shop'
+ 
+
+function App() {
+
+  return (
+    <>
+      {/* <div>hello i am app file</div> */}
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/shop' element={<Shop />} />
+          </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <header className='shadow'>
+      <button className='btn btn-primary'> click me</button>
+    </header>
+  )
+}
+
+export default Home
